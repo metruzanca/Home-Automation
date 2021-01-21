281f47481a01ad9627e3dff1e6087f1bec3a832b
metruzanca <metruzanca@gmail.com>
2021-01-22 00:34:16 +0100

    fixed path issue

--- src/nodeStart.sh
+++ src/nodeStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running Home-Automation onBoot.js"
-/home/ubuntu/.nvm/versions/node/v14.15.4/bin/node /home/ubuntu/home-automation/src/onBoot.js
+/home/ubuntu/.nvm/versions/node/v14.15.4/bin/node /opt/home-automation/src/onBoot.js
