6fa70eeb7b9dcea60d22f6204c5474e5916a38b5
metruzanca <metruzanca@gmail.com>
2021-01-22 00:49:01 +0100

    env var issues

--- src/nodeStart.sh
+++ src/nodeStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running Home-Automation onBoot.js"
 source /opt/home-automation/.env
-/home/ubuntu/.nvm/versions/node/v14.15.4/bin/node /opt/home-automation/src/onBoot.js
+WEBHOOK_TOKEN=$WEBHOOK_TOKEN /home/ubuntu/.nvm/versions/node/v14.15.4/bin/node /opt/home-automation/src/onBoot.js
